61dc39d572fb46a51405f00d03d7030a8e154f58
cssxsh <cssxsh@gmail.com>
2021-05-13 14:46:53 +0800

    feat: SendLimit

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/Message.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/Message.kt
@@ -1,16 +1,35 @@
 package xyz.cssxsh.mirai.plugin
 
+import kotlinx.coroutines.delay
 import net.mamoe.mirai.console.command.CommandSenderOnMessage
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.message.data.Message
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
+import net.mamoe.mirai.message.data.toPlainText
+import kotlin.time.ExperimentalTime
+import kotlin.time.minutes
 
+val SendLimit = """本群每分钟只能发\d+条消息""".toRegex()
+
+@ExperimentalTime
 suspend fun <T : CommandSenderOnMessage<*>> T.sendMessage(block: suspend T.(Contact) -> Message): Boolean {
     return runCatching {
         block(fromEvent.subject)
-    }.onSuccess {
-        sendMessage(fromEvent.message.quote() + it)
+    }.onSuccess { message ->
+        quoteReply(message)
     }.onFailure {
-        sendMessage(fromEvent.message.quote() + it.toString())
+        when {
+            SendLimit.containsMatchIn(it.message.orEmpty()) -> {
+                delay((1).minutes)
+                quoteReply(SendLimit.find(it.message!!)!!.value)
+            }
+            else -> {
+                quoteReply("发送消息失败， ${it.message}")
+            }
+        }
     }.isSuccess
 }
+
+suspend fun CommandSenderOnMessage<*>.quoteReply(message: Message) = sendMessage(fromEvent.message.quote() + message)
+
+suspend fun CommandSenderOnMessage<*>.quoteReply(message: String) = quoteReply(message.toPlainText())
