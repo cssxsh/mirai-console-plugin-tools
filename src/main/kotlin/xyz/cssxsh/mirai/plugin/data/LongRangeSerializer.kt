4b2c42709c7edc7a5e7761c93300ee5343e86a3c
cssxsh <cssxsh@gmail.com>
2021-05-14 01:07:42 +0800

    feat: serializer

---
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/data/LongRangeSerializer.kt
@@ -0,0 +1,24 @@
+package xyz.cssxsh.mirai.plugin.data
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.LongArraySerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.buildSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+@InternalSerializationApi
+@ExperimentalSerializationApi
+object LongRangeSerializer : KSerializer<LongRange> {
+    override val descriptor: SerialDescriptor =
+        buildSerialDescriptor(LongRange::class.qualifiedName!!, StructureKind.LIST)
+
+    override fun deserialize(decoder: Decoder): LongRange =
+        decoder.decodeSerializableValue(LongArraySerializer()).let { (first, last) -> first..last }
+
+    override fun serialize(encoder: Encoder, value: LongRange) =
+        encoder.encodeSerializableValue(LongArraySerializer(), listOf(value.first, value.last).toLongArray())
+}
