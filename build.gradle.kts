9fb8514ac30f5bf8eac299b2dc0e14f8f6482a7f
cssxsh <cssxsh@gmail.com>
2021-05-16 10:53:24 +0800

    build: ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,9 @@ version = "0.1.0"
 
 repositories {
     mavenLocal()
+    // gradle-plugin
+    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
     gradlePluginPortal()
-    maven(url = "https://maven.aliyun.com/repository/releases")
-    maven(url = "https://mirrors.huaweicloud.com/repository/maven")
-    // bintray dl.bintray.com -> bintray.proxy.ustclug.org
-    maven(url = "https://bintray.proxy.ustclug.org/him188moe/mirai/")
-    maven(url = "https://bintray.proxy.ustclug.org/kotlin/kotlin-dev")
-    maven(url = "https://bintray.proxy.ustclug.org/kotlin/kotlinx/")
     // central
     maven(url = "https://maven.aliyun.com/repository/central")
     mavenCentral()
