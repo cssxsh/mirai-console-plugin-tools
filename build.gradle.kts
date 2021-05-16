8ad6849a2d21f1559b267951dd082d864148fc59
cssxsh <cssxsh@gmail.com>
2021-05-16 11:08:43 +0800

    build: ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,12 @@ version = "0.1.0"
 
 repositories {
     mavenLocal()
-    // gradle-plugin
-    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
-    gradlePluginPortal()
-    // central
     maven(url = "https://maven.aliyun.com/repository/central")
-    mavenCentral()
-    // jcenter
     maven(url = "https://maven.aliyun.com/repository/jcenter")
+    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+    mavenCentral()
     jcenter()
+    gradlePluginPortal()
 }
 
 dependencies {
